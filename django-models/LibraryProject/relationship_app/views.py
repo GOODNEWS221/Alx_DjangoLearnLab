c50d86b75ab989f1dd50ef989b2c73ed12580acf
GoodnewsAtekha <goodnewsatekha@gmail.com>
2025-07-21 02:40:35 +0100

    changes

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -6,7 +6,33 @@ from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 
 from .models import Book, Library
 from .forms import RegisterForm  # Use if you have a custom registration form
+from django.shortcuts import render
+from django.contrib.auth.decorators import permission_required
+from django.contrib.auth.decorators import login_required, user_passes_test
+from django.shortcuts import render
 
+# Check if user is in Admin group
+def is_admin(user):
+    return user.groups.filter(name='Admin').exists()
+
+# Admin-only view
+@login_required
+@user_passes_test(is_admin)
+def admin_view(request):
+    return render(request, 'admin_dashboard.html')
+
+
+@permission_required('relationship_app.can_add_book', raise_exception=True)
+def add_book(request):
+    return render(request, 'add_book.html')
+
+@permission_required('relationship_app.can_change_book', raise_exception=True)
+def edit_book(request):
+    return render(request, 'edit_book.html')
+
+@permission_required('relationship_app.can_delete_book', raise_exception=True)
+def delete_book(request):
+    return render(request, 'delete_book.html')
 # ========================
 # Role Check Functions
 # ========================
