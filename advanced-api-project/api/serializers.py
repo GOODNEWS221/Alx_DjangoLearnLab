3f7b35fd96561e64ee5b5df8299c45621dc77d55
GoodnewsAtekha <goodnewsatekha@gmail.com>
2025-08-08 19:45:04 +0100

    My DRF project

---
+++ advanced-api-project/api/serializers.py
@@ -0,0 +1,26 @@
+from rest_framework import serializers
+from .models import Author, Book
+import datetime
+
+# Serializer for the Book model including validation logic
+class BookSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Book
+        fields = '__all__'
+
+    def validate_publication_year(self, value):
+        current_year = datetime.date.today().year
+        if value > current_year:
+            raise serializers.ValidationError("Publication year cannot be in the future.")
+        return value
+
+# Serializer for Author model, embedding BookSerializer for nested representation
+class AuthorSerializer(serializers.ModelSerializer):
+    books = BookSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Author
+        fields = ['name', 'books']
+
+
+        
